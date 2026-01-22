17536dd10cf22748d443a01908e398138bed851f
medchahid2024 <medchahid2022@gmail.com>
2026-01-22 12:19:53 +0100

    onfigurer les API via les vichiers env

--- Frontend/src/API/apiClient.js
+++ Frontend/src/API/apiClient.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/",
-    aders: { "Content-Type": "application/json" },
+    baseURL: process.env.REACT_APP_API_BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 export default api;
